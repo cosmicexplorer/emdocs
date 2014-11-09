e04d445ebe20bbd98d457e97c2494eaa33dcbd4a
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-11-09 03:23:15 -0600

    wreckage

--- test_base.js
+++ test_base.js
@@ -2,21 +2,17 @@ var p2p = require('./p2p.js');
 
 // process.argv[2]: the ip to connect to
 
-// var p = new p2p.peer("http://" + process.argv[2] + ":8080", 8080);
-
 var p = new p2p.peer("http://" + process.argv[2] + ":8080", 8080);
+// var p = new p2p.server(8080);
+
 p.start(
   function() {
     setInterval(function() {
       console.log("----\nclient users:");
-      // p.client.socketTable.forEach(function(socketUri) {
       p.client.socketTable.forEach(function(socketUri) {
         console.log("\t" + socketUri);
       });
     }, 10000);
-    // DIRTY HACKS, SCREW EC2
-    p.client.selfGlobalUri = "http://54.86.46.156:8080";
-    // p.selfGlobalUri = "http://54.86.46.156:8080";
   },
   null,
   function() {
