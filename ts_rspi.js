94e05ebe87742cba1a4f7bc221dd68bcbfef5662
Daniel McClanahan <danieldmcclanahan@gmail.com>
2014-11-09 02:37:24 -0700

    file transfer

---
+++ ts_rspi.js
@@ -0,0 +1,27 @@
+var p2p = require('./p2p.js');
+
+// process.argv[2]: the ip to connect to
+
+// var p = new p2p.peer("http://" + process.argv[2] + ":8080", 8080);
+
+var p = new p2p.peer("http://" + process.argv[2] + ":8080", 8080);
+p.start(
+  function() {
+    setInterval(function() {
+      console.log("----\nclient users:");
+      // p.client.socketTable.forEach(function(socketUri) {
+      p.client.socketTable.forEach(function(socketUri) {
+        console.log("\t" + socketUri);
+      });
+    }, 10000);
+  },
+  null,
+  function() {
+    setInterval(function() {
+      console.log("----\nserver users:");
+      p.server.socketTable.forEach(function(socketUri) {
+        console.log("\t" + socketUri);
+      });
+    }, 10000);
+  },
+  null);
