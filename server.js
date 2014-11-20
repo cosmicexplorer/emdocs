1de6097dffb988238f7a88611a9468a32f489d6a
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-11-20 17:28:55 -0600

    maybe fixing things

--- server.js
+++ server.js
@@ -86,9 +86,9 @@ loadEmacsLisp(utilities.LISP_FILE_PATH, function() {
           ':' +
           utilities.SERVER_HTTP_PORT);
         if ("127.0.0.1" == process.argv[3]) { // if initial server
-          setInterval(broadcastBuffer, utilities.FILE_SYNC_TIME);
+          // setInterval(broadcastBuffer, utilities.FILE_SYNC_TIME);
+          setInterval(broadcastDiff, utilities.DIFF_SYNC_TIME);
         }
-        setInterval(broadcastDiff, utilities.DIFF_SYNC_TIME);
       },
       // server socket function
       function(socket) {
