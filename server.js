fcc72d3a2f21d76dd556b08428b7d57385afbaf0
Danny McClanahan <danieldmcclanahan@gmail.com>
2014-11-24 08:05:02 -0600

    still can't get bidir diff working

--- server.js
+++ server.js
@@ -98,10 +98,10 @@ loadEmacsLisp(utilities.LISP_FILE_PATH, function() {
       function() {
         console.log("listening on " + utilities.os.hostname() + ':' +
           utilities.SERVER_HTTP_PORT);
-        // if (process.argv[3] == "127.0.0.1") {
+        if (process.argv[3] == "127.0.0.1") {
         setInterval(broadcastDiff, utilities.DIFF_SYNC_TIME);
         setInterval(broadcastBuffer, utilities.FILE_SYNC_TIME);
-        // }
+        }
       },
 
       // server socket function
